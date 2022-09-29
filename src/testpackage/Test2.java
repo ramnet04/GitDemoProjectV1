040bab35fe333a1ceb10620ba0cc9c2fdb495eb6
test <test@Mydevice>
2022-09-30 05:17:04 +0530

    Updated Test2 with multiple conditions

--- src/testpackage/Test2.java
+++ src/testpackage/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		String hungry = "yes";
 		
-		if (hungry == "yes") {
+		if (hungry == "yes" || hungry == "YES" || hungry == "Yes") {
 			System.out.println("eat something....");
 		}else {
 			System.out.println("Do work....");
